058a4902cc3ba850a3f4ba567fbef4248bec7009
TranAnh-Tuan <18207035@student.hcmus.edu.vn>
2022-09-19 20:02:06 +0700

    First Commit

---
+++ src/Component/SignUp.js
@@ -0,0 +1,20 @@
+import './Signup.css'
+import React from "react";
+
+const SignUp = () =>{
+    const bar = (
+        <div className={"background-box"}>
+            <div  className={"first-box"} >
+                <div className={"hexagon"}>
+
+                </div>
+            </div>
+        </div>
+    );
+    return (
+        <div>
+            {bar}
+        </div>
+    );
+}
+export default SignUp;
