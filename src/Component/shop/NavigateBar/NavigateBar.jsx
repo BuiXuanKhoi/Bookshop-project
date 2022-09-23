0fc8e91d3dabdd7bc23ac34f0266241c1f5de07f
TranAnh-Tuan <18207035@student.hcmus.edu.vn>
2022-09-23 23:30:46 +0700

    Modify header bar v1.0

--- src/Component/shop/NavigateBar/NavigateBar.jsx
+++ src/Component/shop/NavigateBar/NavigateBar.jsx
@@ -1,14 +1,73 @@
 import React from "react";
 import "./NavigateBar.css"
 import {BookFilled} from "@ant-design/icons";
+import {Button, Dropdown, Menu} from "antd";
+import {UserOutlined,ProfileOutlined,HourglassOutlined,LogoutOutlined} from "@ant-design/icons";
+
+const menu = (
+    <Menu
+        items={[
+            {
+                key: '1',
+                label: (
+                    <div>
+                        <ProfileOutlined className={"icon-user"}/>
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com" className={"information"}>
+                            Personal's Information
+                        </a>
+                    </div>
+                ),
+            },
+            {
+                key: '2',
+                label: (
+                    <div>
+                        <HourglassOutlined className={"icon-history"}/>
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com" className={"history"}>
+                            Ordered history
+                        </a>
+                    </div>
+                ),
+            },
+            {
+                key: '3',
+                label: (
+                    <div>
+                        <LogoutOutlined className={"icon-logout"}/>
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com" className={"logout"}>
+                            Log out
+                        </a>
+                    </div>
+                ),
+            },
+        ]}
+    />
+);
 export default function NavigateBar() {
     return(
-
+        <>
         <div className={"nah"}>
-
             <BookFilled className={"book-icon"}/>
-            <p className={"text-next-to-icon"}>Bookshop</p>
-        </div>
+            <p className={"text-next-to-icon"}>Book Shop</p>
+            <ul>
+                <li>Home</li>
+                <li>Shop</li>
+                <li style={{textDecoration:"underline",}}>About</li>
+                <li>Cart</li>
+                <li>Sign in/Sign up</li>
+            </ul>
 
+        </div>
+        <div className={"personal"}>
+            <Dropdown overlay={menu}
+                      placement={"bottomRight"}
+                      arrow={{
+                          pointAtCenter:true,}
+                      }
+            >
+                <Button className={"button-icon"}><UserOutlined className={"icon-user"}/></Button>
+            </Dropdown>
+        </div>
+        </>
     );
 }
