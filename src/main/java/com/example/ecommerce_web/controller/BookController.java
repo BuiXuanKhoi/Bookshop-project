a735bb8e3f32e0e6bd3be8c167367be1f1c2c5a3
Bui Xuan Khoi <khoibuiqn1011@gmail.com>
2022-09-29 21:10:35 +0700

    Update Cros Origin

--- src/main/java/com/example/ecommerce_web/controller/BookController.java
+++ src/main/java/com/example/ecommerce_web/controller/BookController.java
@@ -24,6 +24,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/books")
+@CrossOrigin(maxAge = 3600, origins = "*")
 public class BookController {
 
     BookService bookService;
--- src/main/java/com/example/ecommerce_web/controller/UserController.java
+++ src/main/java/com/example/ecommerce_web/controller/UserController.java
@@ -19,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin(maxAge = 3600, origins = "*")
 public class UserController {
 
     UserService userService;
