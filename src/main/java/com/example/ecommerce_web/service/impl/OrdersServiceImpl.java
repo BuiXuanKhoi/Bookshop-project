ed742a6f29b055a0602273a1e1ae73ed5469822d
Bui Xuan Khoi <khoibuiqn1011@gmail.com>
2022-10-10 19:44:05 +0700

    Update manage order sort by id

--- src/main/java/com/example/ecommerce_web/service/impl/OrdersServiceImpl.java
+++ src/main/java/com/example/ecommerce_web/service/impl/OrdersServiceImpl.java
@@ -20,6 +20,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
